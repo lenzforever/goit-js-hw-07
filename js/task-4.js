8f8ab415c023c58628dd50b94929f87d2f8e1f88
Nikita Boyarinov <niko.0707.niko@gmail.com>
2024-06-26 17:23:21 +0300

    task 4-fix

--- js/task-4.js
+++ js/task-4.js
@@ -5,7 +5,7 @@ logInForm.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
     event.preventDefault();
-    const form = event.target; ю
+    const form = event.target; 
     const email = form.elements.email.value.trim();
     const password = form.elements.password.value.trim();
 }
