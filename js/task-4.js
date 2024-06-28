21dfe85d1948dc9fd9c89c960344a6ebe7808685
Nikita Boyarinov <niko.0707.niko@gmail.com>
2024-06-28 11:43:07 +0300

    task 4-fix

--- js/task-4.js
+++ js/task-4.js
@@ -8,11 +8,11 @@ function handleSubmit(event) {
     const form = event.target; 
     const email = form.elements.email.value.trim();
     const password = form.elements.password.value.trim();
-}
 
-  if (email === '' || password === '') {
-    alert('All form fields must be filled in');
-  } else {
-    console.log({ email, password }); 
-    form.reset(); 
+    if (email === '' || password === '') {
+        alert('All form fields must be filled in');
+    } else {
+        console.log({ email, password });
+        form.reset();
+    }
 }
